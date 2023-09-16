f31bd863af10e3ee0ef40e0edeb35ae3318527d3
thushalya <weeeratharava@gmail.com>
2023-09-16 12:56:00 +0530

    code

---
+++ build.bat
@@ -0,0 +1,20 @@
+@echo off
+
+:: Remove the dist directories if they already exist
+if exist dist_mv3 rmdir /s /q dist_mv3
+if exist dist_mv2 rmdir /s /q dist_mv2
+
+:: Create the dist directories
+mkdir dist_mv3
+mkdir dist_mv2
+
+:: Copy the files from "src" to "dist_mv3" and "dist_mv2"
+xcopy /s /e src\* dist_mv3
+xcopy /s /e src\* dist_mv2
+
+:: Copy the manifest files to their respective dist directories
+copy manifests\mv3-manifest\manifest.json dist_mv3
+copy manifests\mv2-manifest\manifest.json dist_mv2
+
+REM
+pause
---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Remove the dist directories if they already exist
+rm -rf dist_mv3 dist_mv2
+
+# Create the dist directories
+mkdir -p dist_mv3 dist_mv2
+
+# Copy the files from "src" to "dist_mv3" and "dist_mv2"
+cp -r src/* dist_mv3
+cp -r src/* dist_mv2
+
+# Copy the manifest files to their respective dist directories
+cp manifests/mv3-manifest/manifest.json dist_mv3
+cp manifests/mv2-manifest/manifest.json dist_mv2
+
+zip -r -FS dist_mv3.zip dist_mv3
+
+cd dist_mv2
+zip -r -FS ../dist_mv2.zip *
+cd ..
+
+mv dist_mv3.zip ~/Downloads
+mv dist_mv2.zip ~/Downloads
